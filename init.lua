b3b19d6c7af8944e009b52edc26c472e223fe656
Rami Jebara <rami.jebara@hyperviewhq.com>
2026-05-26 18:19:29 -0700

    enable rust_analyzer

--- init.lua
+++ init.lua
@@ -693,7 +693,7 @@ do
     -- clangd = {},
     -- gopls = {},
     -- pyright = {},
-    -- rust_analyzer = {},
+    rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
